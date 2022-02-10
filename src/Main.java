9f354c23e8ddc70a98183e114c639a94eff6aca1
Anastas Metaxa <metaxaanastas@mail.ru>
2022-02-10 12:23:02 +0600

    Description added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 7_019; //Стоимость билета в рублях
+        int ticketPrice = 10_000; //Стоимость билета в рублях
         int minimalAmount = 20;  //Минимальное количество рублей для получения одной бонусной мили
 
         int bonusCalculation = ticketPrice / minimalAmount; //Расчет бонусных миль
